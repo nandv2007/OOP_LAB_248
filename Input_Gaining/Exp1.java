577e1cb7fe5f92722cb374e10db37ded7a6e2a82
nandv2007 <nandhinivenkatesan07@gmail.com>
2025-12-22 19:53:24 +0530

    Added Java basic codes

---
+++ Input_Gaining/Exp1.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Exp1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char a=sc.next().charAt(0);
+        int b=sc.nextInt();
+        float c=sc.nextFloat();
+        double d=sc.nextDouble();
+        System.out.println("Character: "+a);
+        System.out.println("Integer: "+b);
+        System.out.println("Float: "+c);
+        System.out.println("Double: "+d);
+        sc.close();
+    }
+
+    
+}
