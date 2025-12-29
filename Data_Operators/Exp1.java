f457cee9e3929c671277a8ea5219c8c2681c9015
Nandhini Venkatesan <nandhinivenkatesan07@gmail.com>
2025-12-29 12:47:48 +0530

    Add files via upload

---
+++ Data_Operators/Exp1.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Exp1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+	System.out.println("Simple Calculator");
+        double a;
+        double b;
+        System.out.print("Enter first number: ");
+        a = sc.nextDouble();
+        sc.nextLine();
+        System.out.print("Enter second number: ");
+        b = sc.nextDouble();
+        sc.nextLine();
+	double c=a+b;
+	double d=a-b;
+	double e=a*b;
+	double f=a/b;     
+        System.out.println("Sum: "+c);
+        System.out.println("Difference: "+d);
+        System.out.println("Product: "+e);
+        System.out.println("Division: "+f);
+        sc.close();
+    }
+}
