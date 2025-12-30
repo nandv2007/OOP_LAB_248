01573c61fbe82af763ed45522e0fc86536f90dbe
Nandhini Venkatesan <nandhinivenkatesan07@gmail.com>
2025-12-30 19:19:50 +0530

    Format and clean up Exp2.java

--- Data_Operators/Exp2.java
+++ Data_Operators/Exp2.java
@@ -2,11 +2,10 @@ import java.util.Scanner;
 public class Exp2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-	System.out.println("Greatest Number");
+		System.out.println("Greatest Number");
         int a;
         int b;
         int c;
-
         System.out.print("Enter first number: ");
         a = sc.nextInt();
         sc.nextLine();
@@ -16,12 +15,13 @@ public class Exp2 {
         System.out.print("Enter third number: ");
         c = sc.nextInt();
         sc.nextLine();
-	if(a>b && a>c){
-	System.out.println("a is greatest number");
-	}else if(b>a && b>c){
-	System.out.println("b is greatest number");
-	}else{
-	System.out.println("c is greatest number");
-	}
+		if(a>b && a>c){
+			System.out.println("a is greatest number");
+		}else if(b>a && b>c){
+			System.out.println("b is greatest number");
+		}else{
+			System.out.println("c is greatest number");
+		}
     }
+
 }
