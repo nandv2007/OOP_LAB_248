8bd8b4b51d6eabe4bb48bede205c78167c460be7
Nandhini Venkatesan <nandhinivenkatesan07@gmail.com>
2026-01-01 21:45:20 +0530

    Delete Output_Printing directory

--- Output_Printing/Exp2.java
+++
@@ -1,9 +0,0 @@
-public class Exp2 {
-    public static void main(String[] args) {
-        System.out.println("Welcome to OOP Lab!");
-        int a = 5;
-        int b = 10;
-        int sum = a + b;
-        System.out.println("The sum of " + a + " and " + b + " is: " + sum);
-    }
-}
