d7039e3a2991884048553204e1555a13d8563385
nandv2007 <nandhinivenkatesan07@gmail.com>
2026-01-01 22:43:35 +0530

    Updation

--- 3. Data_Operators (LS 2)/Exp3.java
+++ 3. Data_Operators (LS 2)/Exp3.java
@@ -1,19 +1,22 @@
 import java.util.Scanner;
-public class Exp3{
-    public static void main(String[] args){
-	Scanner sc= new Scanner(System.in);
-        System.out.println("Assignment Operation");
-	System.out.print("Enter a number: ");
-        int n = sc.nextInt();
-        n += 5;  
-        System.out.println("After n+=5 : " + n);
-        n *= 4;  
-        System.out.println("After n*=4 : " + n);
-        n -= 3;
-        System.out.println("After n-=3 : " + n);
-        n /= 2;  
-        System.out.println("After n/=2 : " + n);
-        n %= 6;
-        System.out.println("After n%=6 : " + n);
+
+public class Exp3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+        System.out.println("\n=== Relational Operators ===");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println();
+        System.out.println("a == b: " + (a == b));
+        System.out.println("a != b: " + (a != b));
+        System.out.println("a > b: " + (a > b));
+        System.out.println("a < b: " + (a < b));
+        System.out.println("a >= b: " + (a >= b));
+        System.out.println("a <= b: " + (a <= b));
+        sc.close();
     }
 }
