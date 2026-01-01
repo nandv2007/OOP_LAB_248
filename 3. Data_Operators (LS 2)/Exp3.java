d3df06d255d288f20910397959b6722dfea8272c
nandv2007 <nandhinivenkatesan07@gmail.com>
2026-01-01 22:47:23 +0530

    (29.12.2025) Lab Session 2- Data Types and Operators

--- 3. Data_Operators (LS 2)/Exp3.java
+++ 3. Data_Operators (LS 2)/Exp3.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
-
 public class Exp3 {
-    public static void main(String[] args) {
+        public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter first number: ");
         int a = sc.nextInt();
